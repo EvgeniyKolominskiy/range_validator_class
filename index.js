6b0e56eefbd5904c32ababa01baeee293af1c48b
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-07-13 19:00:52 +0300

    Added project skeleton.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 'use strict';
+
+class RangeValidator {
+	constructor(from, to) {
+		this._from = from;
+		this._to = to;
+	}
+
+	set from(v) {
+
+	}
+
+	get from() {
+
+	}
+
+	set to(v) {
+
+	}
+
+	get to() {
+
+	}
+
+	get range() {
+
+	}
+
+	validate(v) {
+
+	}
+}
