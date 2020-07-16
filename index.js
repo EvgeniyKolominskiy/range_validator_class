a1ae8320ed7b401148145481e1d4d9ab9216d452
Evgeniy Kolominskiy <kolominskiyy@gmail.com>
2020-07-16 21:32:36 +0300

    Finish range validator task.

--- index.js
+++ index.js
@@ -7,26 +7,39 @@ class RangeValidator {
 	}
 
 	set from(v) {
-
+		if (typeof v !== 'number') {
+			throw new TypeError('Value from is not a number.');
+		}
+		this._from = v;
 	}
 
 	get from() {
-
+		return this._from;
 	}
 
 	set to(v) {
-
+		if (typeof v !== 'number') {
+			throw new TypeError('Value to is not a number.');
+		}
+		return this._to = v;
 	}
 
 	get to() {
-
+		return this._to;
 	}
 
 	get range() {
-
+		return [this.from, this.to];
 	}
 
-	validate(v) {
-
+	validate(number) {
+		return (number >= this.from && number <= this.to);
 	}
 }
+
+const rangeVal = new RangeValidator(50, 100);
+
+console.log(rangeVal.range);
+console.log(rangeVal.validate(100));
+console.log(rangeVal.validate(55));
+console.log(rangeVal.validate(-7));
